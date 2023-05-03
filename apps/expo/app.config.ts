7426c21381c407f61a75472cfe7fc1072a1bef1a
yumip <71173724+yumip@users.noreply.github.com>
2023-05-03 10:30:12 +1000

    Initialize expo-router

---
+++ apps/expo/app.config.ts
@@ -0,0 +1,42 @@
+import type { ExpoConfig } from "@expo/config";
+
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_cHJvLWNyYXdkYWQtODcuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
+const defineConfig = (): ExpoConfig => ({
+  name: "expo",
+  slug: "expo",
+  scheme: "expo",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#1F104A",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "your.bundle.identifier",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/icon.png",
+      backgroundColor: "#1F104A",
+    },
+  },
+  extra: {
+    eas: {
+      projectId: "your-project-id",
+    },
+    CLERK_PUBLISHABLE_KEY,
+  },
+  plugins: ["./expo-plugins/with-modify-gradle.js"],
+});
+
+export default defineConfig;
